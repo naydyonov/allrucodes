055e970ae81a7c255d4a4094a15d236f9e86b6bb
Alexey Naydenov <anaydenov@MacBook-Pro-Alexey-2.local>
2022-03-22 11:43:28 +0300

    data and refactor structure and search

--- allrucodes/__init__.py
+++ allrucodes/__init__.py
@@ -4,5 +4,5 @@ __author__ = """Alexey Naydyonov"""
 __email__ = 'a.naydyonov@gmail.com'
 __version__ = '0.1.0'
 
-from okopf import OKOPF
-from oksm import OKSM
+from .oksm import OKSMCodes
+from .okopf import OKOPFCodes
