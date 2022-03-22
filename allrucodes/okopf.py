1206ade675e5bdcc305f2f0d27b7de76dac43460
Alexey Naydenov <anaydenov@MacBook-Pro-Alexey-2.local>
2022-03-22 14:57:46 +0300

    formatting

--- allrucodes/okopf.py
+++ allrucodes/okopf.py
@@ -3,5 +3,6 @@ from .basecodes import BaseCodes
 
 class OKOPFCodes(BaseCodes):
     def __init__(self) -> None:
-        super().__init__('okopf.pkl',
-                         dict_search_fields=['name'])
+        super().__init__(
+            "okopf.pkl", dict_search_fields=["name"], partial_search_fields=["name"]
+        )
